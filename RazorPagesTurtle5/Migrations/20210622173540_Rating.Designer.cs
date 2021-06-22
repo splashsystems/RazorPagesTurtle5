53cb17c24cf75904626708a3b699bd24fe2d0d1d
Scot <bscot.hagen@gmail.com>
2021-06-22 10:36:30 -0700

    Added new field, Rating

--- RazorPagesTurtle5/Migrations/20210622173540_Rating.Designer.cs
+++ RazorPagesTurtle5/Migrations/20210622173540_Rating.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesTurtle5.Data;
 
 namespace RazorPagesTurtle5.Migrations
 {
     [DbContext(typeof(RazorPagesTurtle5Context))]
-    partial class RazorPagesTurtle5ContextModelSnapshot : ModelSnapshot
+    [Migration("20210622173540_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RazorPagesTurtle5/Migrations/20210622173540_Rating.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesTurtle5.Migrations
+{
+    public partial class Rating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Turtle",
+                type: "decimal(18, 2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Turtle",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Turtle");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Turtle",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18, 2)");
+        }
+    }
+}
